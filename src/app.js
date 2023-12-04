b1c610fdaf6a85be5777ab18a5555c3eac17f85b
Michael Di Giuseppe <mdigius@uwo.ca>
2023-12-04 15:54:14 -0500

    Template MYSQL Query for api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,26 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 
+const {
+    createPool
+} = require('mysql2')
+
+const pool = createPool({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'rootuser',
+    database: 'sys',
+    connectionLimit: 10
+
+})
+
+// pool.query('SELECT * FROM User', [], (err, result, fields) => {
+//     if(err){
+//         return console.log(err)
+//     }
+//     return console.log(result)
+// })
 // Code to kill port: lsof -ti:5002 | xargs kill -9
  
 const app = express();
