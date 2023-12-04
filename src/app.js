49c3c72d38f02ebcea6bfb7b999ae0a901d78b94
Michael Di Giuseppe <mdigius@uwo.ca>
2023-12-04 15:38:18 -0500

    App.js update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
-const sanitizeHtml = require('sanitize-html');
-const {MongoClient} = require('mongodb')
-const bcrypt = require('bcrypt');
-const fuzzy = require('fuzzy')
-const saltRounds = 10;
+
 // Code to kill port: lsof -ti:5002 | xargs kill -9
  
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
 
