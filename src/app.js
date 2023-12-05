03775ae94a85c0f58a80d5909a06417cf5ecbfd0
Michael Di Giuseppe <mdigius@uwo.ca>
2023-12-04 21:19:08 -0500

    Committed forums, comments, and api changes

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ app.route('/api/secure/forums')
     .get((req, res) => {
         const results = [];
         
-        pool.query('SELECT * FROM Forum', [], (err, result, fields) => {
+        pool.query('SELECT * FROM Forum ORDER BY RAND() LIMIT 6', [], (err, result, fields) => {
             if (err) {
                 return res.status(404).json({ error: 'No forums in the database' });
             } else {
@@ -39,6 +39,35 @@ app.route('/api/secure/forums')
             }
         });
     });
-
+    app.route('/api/secure/forums/:forumID/comments')
+    .get((req, res) => {
+        const results = [];
+        const forumID = req.params.forumID
+        
+        pool.query(`SELECT * FROM Comment WHERE forumID = ?`, [forumID], (err, result, fields) => {
+            if (err) {
+                return res.status(404).json({ error: 'No forums in the database' });
+            } else {
+                // Assuming result is an array, you may need to adjust accordingly
+                results.push(...result);
+                res.json(results);
+            }
+        });
+    });
+    app.route('/api/secure/comments/:commentID/children')
+    .get((req, res) => {
+        const results = [];
+        const commentID = req.params.commentID
+        
+        pool.query(`SELECT * FROM Comment WHERE parentCommentID = ?`, [commentID], (err, result, fields) => {
+            if (err) {
+                return res.status(404).json({ error: 'No children comment' });
+            } else {
+                // Assuming result is an array, you may need to adjust accordingly
+                results.push(...result);
+                res.json(results);
+            }
+        });
+    });
 
 app.listen(5002, () => console.log('Listening on port 5002'));
--- src/components/ForumComponent.tsx
+++ src/components/ForumComponent.tsx
@@ -1,11 +1,37 @@
 import { ForumComponentProps } from '@/types'
 import { Card } from 'flowbite-react'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import CommentComponent from './CommentComponent'
 
 
 const ForumComponent = ({forumData}: ForumComponentProps) => {
+    const [comments, setComments] = useState([])
 
     async function fetchComments(){
+        const url = `http://localhost:5002/api/secure/forums/${forumData.forumID}/comments`;
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(responseData => {
+                console.log('GET successful:', responseData);
+                setComments(responseData)
+
+                
+                
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                
+            });
 
     }
 
@@ -13,7 +39,7 @@ const ForumComponent = ({forumData}: ForumComponentProps) => {
         fetchComments()
     }, [])
   return (
-    <Card>
+    <Card className = "max-w-xl">
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {forumData.title}
         </h5>
@@ -23,6 +49,14 @@ const ForumComponent = ({forumData}: ForumComponentProps) => {
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Comments:
         </h5>
+        <div className='results'>
+        {comments.map((result) => (
+          <div className="mt-10">            
+            <CommentComponent commentData={result}/>
+         </div>
+         ))}  
+         </div>
+
 
     </Card>
   )
