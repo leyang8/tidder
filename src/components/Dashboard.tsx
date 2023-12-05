fbf2ad312de68ac0359179fb592ae24700abb29b
LEYANG XING <lxing24@uwo.ca>
2023-12-05 15:38:03 -0500

    Committed currentUser showing(get cookie) and logout

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,11 @@
 import { Button } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { ForumComponent } from '.';
+import Cookies from 'js-cookie';
 
 const Dashboard = () => {
     const [forums, setForums] = useState([]);
+    const [currentUsername, setCurrentUsername] = useState<string>("")
 
     async function fetchForums(){
         const url = `http://localhost:5002/api/secure/forums`;
@@ -22,9 +24,6 @@ const Dashboard = () => {
             .then(responseData => {
                 console.log('GET successful:', responseData);
                 setForums(responseData)
-
-                
-                
             })
             .catch(error => {
                 console.error('Error:', error);
@@ -32,15 +31,28 @@ const Dashboard = () => {
             });
         
     }
+
+    const onLogout = () => {
+        Cookies.remove('currentUser');
+        window.location.href = '/'
+    }
     useEffect(() => {
-        fetchForums()
+        // Get the username from cookies
+        const current = Cookies.get("currentUser");
+        if (current) {
+            setCurrentUsername(current)
+        }
+        fetchForums();
     }, [])
   return (
     <div className="hero">
     <div className="flex-1 pt-36 padding-x">
-        <h1 className="hero__title">
-            Tidder
-        </h1>
+        <div className="infoContainer">
+            <h1 className="hero__title">
+                Tidder, Welcome! {currentUsername}
+            </h1>
+            <button id='logoutBtn' onClick={onLogout}>Logout</button>
+        </div>
         <p>
             Welcome to Tidder! View forums below!
         </p>
