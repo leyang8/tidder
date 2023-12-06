8c2b95b02df6cb9658843d717a147ddbef4a8970
Jiancheng Luo <ivesljc@gmail.com>
2023-12-05 20:45:29 -0500

    CreateForum is now working

--- src/components/CreateForum.tsx
+++ src/components/CreateForum.tsx
@@ -1,4 +1,4 @@
-import { HTMLFormMethod } from '@remix-run/router';
+"use client";
 import React, { FormEvent, useState } from 'react';
 import Cookies from 'js-cookie';
 
@@ -62,28 +62,25 @@ const CreateForum = () => {
                             Share your thoughts or start a discussion.
                         </p>
 
-            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="col-span-full">
-                <label
-                  htmlFor="forum-title"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Title
-                </label>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="forum-title"
-                    id="forum-title"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="Enter your forum title here"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                    maxLength={255}
-                    required
-                  />
-                </div>
-              </div>
+                        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div className="col-span-full">
+                                <label htmlFor="forum-title" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Title
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        type="text"
+                                        name="forum-title"
+                                        id="forum-title"
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                        placeholder="Enter your forum title here"
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)}
+                                        maxLength={255}
+                                        required
+                                    />
+                                </div>
+                            </div>
 
                             <div className="col-span-full">
                                 <label htmlFor="forum-content" className="block text-sm font-medium leading-6 text-gray-900">
@@ -105,24 +102,21 @@ const CreateForum = () => {
                         </div>
                     </div>
 
-          <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button
-              type="button"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Post
-            </button>
-          </div>
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                            Cancel
+                        </button>
+                        <button
+                            type="submit"
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Post
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default CreateForum;
