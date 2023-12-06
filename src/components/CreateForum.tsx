37b50e40aee716afc23d939ef576bafa9ead14d7
Jiancheng Luo <ivesljc@gmail.com>
2023-12-05 21:38:15 -0500

    Added character limit

--- src/components/CreateForum.tsx
+++ src/components/CreateForum.tsx
@@ -54,7 +54,7 @@ const CreateForum = () => {
 
     return (
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 my-8">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className='mt-40'>
                 <div className="space-y-12">
                     <div className="border-b border-gray-900/10 pb-12">
                         <h2 className="text-base font-semibold leading-7 text-gray-900">Create Forum</h2>
@@ -63,49 +63,52 @@ const CreateForum = () => {
                         </p>
 
                         <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div className="col-span-full">
-                                <label htmlFor="forum-title" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Title
-                                </label>
-                                <div className="mt-2">
-                                    <input
-                                        type="text"
-                                        name="forum-title"
-                                        id="forum-title"
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        placeholder="Enter your forum title here"
-                                        value={title}
-                                        onChange={(e) => setTitle(e.target.value)}
-                                        maxLength={255}
-                                        required
-                                    />
-                                </div>
-                            </div>
+                        <div className="col-span-full">
+                    <label htmlFor="forum-title" className="block text-sm font-medium leading-6 text-gray-900">
+                        Title
+                    </label>
+                    <div className="mt-2">
+                        <input
+                            type="text"
+                            name="forum-title"
+                            id="forum-title"
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="Enter your forum title here"
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            maxLength={255}
+                            required
+                        />
+                        <span className="text-sm text-gray-600">
+                            {title.length}/255
+                        </span>
+                    </div>
+                </div>
 
-                            <div className="col-span-full">
-                                <label htmlFor="forum-content" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Content
-                                </label>
-                                <div className="mt-2">
-                                    <textarea
-                                        id="forum-content"
-                                        name="forum-content"
-                                        rows={6}
-                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                        placeholder="What are you thinking about?"
-                                        value={comment}
-                                        onChange={(e) => setComment(e.target.value)}
-                                        maxLength={255}
-                                    />
-                                </div>
-                            </div>
+                <div className="col-span-full">
+                    <label htmlFor="forum-content" className="block text-sm font-medium leading-6 text-gray-900">
+                        Make the first comment to lead the conversation
+                    </label>
+                    <div className="mt-2">
+                        <textarea
+                            id="forum-content"
+                            name="forum-content"
+                            rows={6}
+                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            placeholder="What are you thinking about?"
+                            value={comment}
+                            onChange={(e) => setComment(e.target.value)}
+                            maxLength={255}
+                        />
+                        <span className="text-sm text-gray-600">
+                            {comment.length}/255
+                        </span>
+                    </div>
+                </div>
                         </div>
                     </div>
 
                     <div className="mt-6 flex items-center justify-end gap-x-6">
-                        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                            Cancel
-                        </button>
                         <button
                             type="submit"
                             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
